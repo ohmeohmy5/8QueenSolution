0baf1290f0acf088479da90171cb541f3f1a87ba
Cesar Ramirez <cramirez3@cpp.edu>
2020-03-01 06:30:42 -0800

    cleaned some stuff up

--- src/CS4200/Board.java
+++ src/CS4200/Board.java
@@ -1,14 +1,14 @@
 package CS4200;
 
 import javafx.application.Application;
-import javafx.geometry.HPos;
-import javafx.geometry.VPos;
-import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Control;
 import javafx.scene.layout.*;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
 
 public class Board extends Application {
 
@@ -19,7 +19,7 @@ public class Board extends Application {
         board.gameLoop(board.getBoard(), 0, 0);
         int[][] boardArray = board.getBoard();
         GridPane boardPane = new GridPane();
-        int size = 8 ;
+        int size = 8;
         Image queenImage = new Image(getClass().getResourceAsStream("queen.png"));
 
         for (int row = 0; row < size; row++)
@@ -31,23 +31,21 @@ public class Board extends Application {
                 queenImageView.setFitHeight(50);
 
                 Pane square = new Pane();
-                String color;
                 if ((row + col) % 2 == 0)
                 {
-                    color = "white";
+                    square.setStyle("-fx-background-color: white");
                 }
                 else
                 {
-                    color = "black";
+                    square.setStyle("-fx-background-color: black");
                 }
-
-                square.setStyle("-fx-background-color: "+color+";");
 
                 if (boardArray[row][col] == 1)
                 {
                     boardPane.add(queenImageView, col, row);
                 }
-                else {
+                else
+                    {
                     boardPane.add(square, col, row);
                 }
             }
@@ -58,8 +56,9 @@ public class Board extends Application {
             boardPane.getColumnConstraints().add(new ColumnConstraints(10, Control.USE_COMPUTED_SIZE, Double.POSITIVE_INFINITY, Priority.ALWAYS, HPos.CENTER, true));
             boardPane.getRowConstraints().add(new RowConstraints(10, Control.USE_COMPUTED_SIZE, Double.POSITIVE_INFINITY, Priority.ALWAYS, VPos.CENTER, true));
         }
-        primaryStage.setScene(new Scene(boardPane, 600, 600));
-       primaryStage.show();
+        Stage stage = primaryStage;
+        stage.setScene(new Scene(boardPane, 600, 600));
+       stage.show();
 
     }
 
